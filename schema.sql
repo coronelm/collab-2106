d4177d6802f68dd5d43c0d67d49d0550728e84f5
MaruAlmarez08 <24-35142@g.batstate-u.edu.ph>
2025-11-17 21:41:34 +0800

    Maru Almarez inserted 2-3 fee records

--- schema.sql
+++ schema.sql
@@ -1,2 +1,7 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE fees (fees_id INT PRIMARY KEY, student_id INT, amount DECIMAL (10, 2));
+
+INSERT INTO fees (fees_id, student_id, amount) VALUES
+(1, 101, 1234.00),
+(2, 102, 6767.67),
+(3, 103, 1975.75);
