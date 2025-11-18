d09c62130e0bc4652496a3f4cf259d749344fcb5
Elie-van <eliezacorralfsu@gmail.com>
2025-11-18 10:31:12 +0800

    Group9-MENDOZAKEL: Add email column

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE users (user_id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(100), role VARCHAR(100));
 INSERT INTO users (admin VARCHAR(100), student_user VARCHAR(100));
+ALTER TABLE users ADD COLUMN email VARCHAR(100);
