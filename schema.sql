361cb126fde7a00f0a102f10befff2bb76cd9255
Elie-van <eliezacorralfsu@gmail.com>
2025-11-18 10:44:05 +0800

    Group9-MARALIT update users email

--- schema.sql
+++ schema.sql
@@ -2,3 +2,5 @@ CREATE DATABASE school_db; USE school_db;
 CREATE TABLE users (user_id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(100), role VARCHAR(100));
 INSERT INTO users (admin VARCHAR(100), student_user VARCHAR(100));
 ALTER TABLE users ADD COLUMN email VARCHAR(100);
+UPDATE users SET email = 'benjairopogi@gmail.com' WHERE users 
+;
