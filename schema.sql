d87056da754ec1b61ec9bb72c77700a065a9e0cf
Elie-van <eliezacorralfsu@gmail.com>
2025-11-18 10:23:39 +0800

    Group9-QUIZON: Insert admin and student user

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE users (user_id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(100), role VARCHAR(100));
+INSERT INTO users (admin VARCHAR(100), student_user VARCHAR(100));
