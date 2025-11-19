ce65fffd821f2f052dc9ee13e544eaf1d76c8b09
mojica-dev <kylekian0113@gmail.com>
2025-11-19 22:11:40 +0800

    Group 8 Member 1: Mojica created attendance table.

--- schema.sql
+++ schema.sql
@@ -1,1 +1,2 @@
 CREATE DATABASE school_db; USE school_db;
+CREATE TABLE attendance(attendance_id INT PRIMARY KEY, student_id INT, date DATE, status ENUM('present', 'absent') NOT NULL DEFAULT 'present');
