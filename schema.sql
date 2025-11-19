38e72eae95398d4931e7a846c6ab69ec109f4377
KianTemplo <24-38254@g.batstate-u.edu.ph>
2025-11-19 11:56:54 +0800

    MIKKO PATULOT Add head_teacher column

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE departments (dept_id INT PRIMARY KEY, dept_name VARCHAR (50));
 INSERT INTO departments (CICS VARCHAR(50), CABE VARCHAR(50));
+ALTER TABLE departments ADD COLUMN head_teacher VARCHAR(50);
