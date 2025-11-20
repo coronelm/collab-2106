5de960a80b22f7c9e7d47707fb4861aa5394a521
Nina Mae C. Lirag <liragnmc@gmail.com>
2025-11-21 07:56:55 +0800

    edit schema.sql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,2 @@
-CREATE TABLE teachers (id INT PRIMARY KEY, name VARCHAR(100), subject VARCHAR(100))
-CREATE TABLE teachers (id INT PRIMARY KEY, name VARCHAR(100), subject VARCHAR(100))
+CREATE DATABASE school_db; USE school_db;
+CREATE TABLE teachers (id INT PRIMARY KEY, name VARCHAR(100), subject VARCHAR(100));
