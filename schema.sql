8fa27621a2f1394d3b74c5bd1db9959a83cf2bc5
mpuri-17 <24-30735@g.batstate-u.edu.ph>
2025-11-20 12:16:23 +0800

    AEINSLEY LANDICHO inserted sample classes

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE classes(class_id INT PRIMARY KEY, class_name VARCHAR(50), room_no INT);
+INSERT INTO classes (class_id, class_name, room_no) VALUES (01, DBMS, 101);
