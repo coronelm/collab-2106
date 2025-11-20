bc0f4cdcbb4c7484c37f4d153ffc4e34bf4f4239
mpuri-17 <24-30735@g.batstate-u.edu.ph>
2025-11-20 11:39:03 +0800

    LYNUS KENZLEY A. DA-ANG created classes table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE classes (class_id INT PRIMARY KEY, class_name VARCHAR(50), room_no INT);
 INSERT INTO classes (DBMS VARCHAR(50));
+CREATE TABLE classes(classes_id INT PRIMARY KEY, class_name VARCHAR(50), room_no INT);
