bc4ce01487a218d8544c621794ffd1191acafc83
jamesdee0904 <24-37104@g.batstate-u.edu.ph>
2025-11-20 09:49:25 +0800

    Group 8 Member 3: Dimaano added remarks column.

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE attendance(attendance_id INT PRIMARY KEY, student_id INT, date DATE, status ENUM('present', 'absent') NOT NULL DEFAULT 'present');
 INSERT INTO attendance(attendance_id, student_id, date, status) VALUES (0001, 0001, 2025-11-20, 'present'), (0002, 0002, 2025-11-20, 'present'), (0003, 0003, 2025-11-20, 'present');
+ALTER TABLE attendance ADD COLUMN remarks VARCHAR(100);
