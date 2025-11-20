295218864d363e2a77385c67ccaa5806ba01d268
gwenn-fonte <24-31341@g.batstate-u.edu.ph>
2025-11-20 11:14:13 +0800

    Group 5 member 2: Fonte inserted sample grades

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE grades(grade_id INT PRIMARY KEY, student_id INT, subject_idINT, score INT);
+INSERT INTO grades (grade_id, student_id, subject_id, score) VALUES (01, 123, 101, 96), (02, 456, 202, 92);
