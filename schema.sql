877a49d23bce4f0d0f5d0cfd4a896dc4d1e0f0e6
Gilian01 <24-35688@g.bastate-u.edu.ph>
2025-11-20 11:23:20 +0800

    Group 5 member 3: Lajara added remarks column

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE grades(grade_id INT PRIMARY KEY, student_id INT, subject_idINT, score INT);
 INSERT INTO grades (grade_id, student_id, subject_id, score) VALUES (01, 123, 101, 96), (02, 456, 202, 92);
+ALTER TABLE grades ADD remarks VARCHAR(100);
