556f577ffd015dee0da026781050874651c1b233
tnchzrvgr <vergaratoni783@gmail.com>
2025-11-20 11:50:00 +0800

    Group 5 member 4: Sy Tamco updated the value of remarks

--- schema.sql
+++ schema.sql
@@ -2,3 +2,4 @@ CREATE DATABASE school_db; USE school_db;
 CREATE TABLE grades(grade_id INT PRIMARY KEY, student_id INT, subject_idINT, score INT);
 INSERT INTO grades (grade_id, student_id, subject_id, score) VALUES (01, 123, 101, 96), (02, 456, 202, 92);
 ALTER TABLE grades ADD remarks VARCHAR(100);
+UPDATE grades SET remarks = passed WHERE student_id = 123;
