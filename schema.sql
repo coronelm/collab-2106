cc634d850f49dd36bee837f290c80207efe02bb5
jllnlvrz <alvarezjillianemay012@gmail.com>
2025-11-20 11:29:17 +0800

    Jilliane Alvarez update one payment status

--- schema.sql
+++ schema.sql
@@ -6,3 +6,4 @@ INSERT INTO fees (fees_id, student_id, amount) VALUES
 (2, 102, 6767.67),
 (3, 103, 1975.75);
 ALTER TABLE fees ADD COLUMN payment_status INT;
+UPDATE fees SET payment_status = 'Paid' WHERE fees_id = 2;
