6389816d920a71335e1bf91e4b27cdda3ad46f1d
mpuri-17 <24-30735@g.batstate-u.edu.ph>
2025-11-20 13:06:49 +0800

    MIKAELA PURI updated class room number

--- schema.sql
+++ schema.sql
@@ -2,3 +2,4 @@ CREATE DATABASE school_db; USE school_db;
 CREATE TABLE classes(class_id INT PRIMARY KEY, class_name VARCHAR(50), room_no INT);
 INSERT INTO classes (class_id, class_name, room_no) VALUES (01, DBMS, 101);
 ALTER TABLE classes ADD teacher_id INT, ADD CONSTRAINT fk_teacher FOREIGN KEY (teacher_id) REFERENCES teachers(id);
+UPDATE classes SET room_no = '102' WHERE class_id = 01;
