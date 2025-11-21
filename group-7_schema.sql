4783eaafaebf3a785382ab1971d63ebdd0507f13
khristelm <24-36315@g.batstate-u.edu.ph>
2025-11-22 01:08:30 +0800

    Khristel Mae D. Mission: add units column

--- group-7_schema.sql
+++ group-7_schema.sql
@@ -1,2 +1,3 @@
 CREATE TABLE subjects (subject_id INT AUTO_INCREMENT PRIMARY KEY, subject_name VARCHAR(100));
 INSERT INTO subjects (subject_id, subject_name) VALUES (211, 'DBMS'), (121, 'ACP');
+ALTER TABLE subjects ADD COLUMN units INT;
