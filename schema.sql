ac45580f8ecaf0821feadba721056bb6783bedd5
Aly <gitaly2433110@gmail.com>
2025-11-21 16:28:05 +0800

    Delete schema.sql
    the first step is not in the file, because the last commit before this, is where the first step is

--- schema.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE school_db; USE school_db;
-INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (01, 001, 2101), (02, 002, 2102), (03, 003, 2103); 
-ALTER TABLE users ADD COLUMN school_year VARCAHR(100);
