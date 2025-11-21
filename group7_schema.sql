884dbb373b33ec6cd7061e6465d1920157bc0332
patzc <patlanzm@gmail.com>
2025-11-21 23:19:18 +0800

    group7-Patrick Jerald O. Lana: Created subject table

--- group7_schema.sql
+++ group7_schema.sql
@@ -1,2 +1,3 @@
 CREATE TABLE subjects (subject_id INT AUTO_INCREMENT PRIMARY KEY, subject_name VARCHAR(100));
 CREATE TABLE subjects (subject_id INT AUTO_INCREMENT PRIMARY KEY, subject_name VARCHAR(100));
+CREATE TABLE subjects (subject_id INT AUTO_INCREMENT PRIMARY KEY, subject_name VARCHAR(100));
