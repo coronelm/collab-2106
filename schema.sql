0a01e64d2515c846dc42fc16f16e84de3a28c4ca
liragnmc <liragnmc@gmail.com>
2025-11-21 08:09:18 +0800

    Lirag, added insert statement

--- schema.sql
+++ schema.sql
@@ -1,1 +1,2 @@
 CREATE DATABASE school_db; USE school_db;
+INSERT INTO teachers (id, name, subject) VALUES (1, 'Mr. Santos', 'Mathematics'), (2, 'Ms. Reyes', 'Science'), (3, 'Mrs. Cruz', 'English');
