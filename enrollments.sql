25c3d9d11638ec46b3abd62414c2bad6893c4950
Aly <gitaly2433110@gmail.com>
2025-11-21 16:26:10 +0800

    Delete enrollments.sql
    wrong file name

--- enrollments.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE enrollments (
-    enrollment_id INT AUTO_INCREMENT PRIMARY KEY,
-    student_id INT NOT NULL,
-    class_id INT NOT NULL,
-    FOREIGN KEY (student_id) REFERENCES students(student_id),
-    FOREIGN KEY (class_id) REFERENCES classes(class_id)
-);
