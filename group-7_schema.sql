5aed44da8956ebf55abb0ea408e55f986b4a9006
24-39885@g.batstate-u.edu.ph <24-39885@g.batstate-u.edu.ph>
2025-11-22 02:03:58 +0800

    Medwin Adam S. Villapando updated one subject's unit

--- group-7_schema.sql
+++ group-7_schema.sql
@@ -1,3 +1,4 @@
 CREATE TABLE subjects (subject_id INT AUTO_INCREMENT PRIMARY KEY, subject_name VARCHAR(100));
 INSERT INTO subjects (subject_id, subject_name) VALUES (211, 'DBMS'), (121, 'ACP');
 ALTER TABLE subjects ADD COLUMN units INT;
+UPDATE subjects SET units = 3 WHERE subject_id '444' WHERE subject_name = 'ComNet';
