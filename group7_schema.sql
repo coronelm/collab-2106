3bce371261086650afa7aa646bfa7249429313c4
pat <patlanzm@gmail.com>
2025-11-22 00:03:42 +0800

    Delete group7_schema.sql

--- group7_schema.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE subjects (subject_id INT AUTO_INCREMENT PRIMARY KEY, subject_name VARCHAR(100));
-CREATE TABLE subjects (subject_id INT AUTO_INCREMENT PRIMARY KEY, subject_name VARCHAR(100));
-CREATE TABLE subjects (subject_id INT AUTO_INCREMENT PRIMARY KEY, subject_name VARCHAR(100));
-CREATE TABLE subjects (subject_id INT AUTO_INCREMENT PRIMARY KEY, subject_name VARCHAR(100));
