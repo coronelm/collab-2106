53faf346971a60ff93ea7845cf1a969801ea6a06
keysie-gif <24-31534.batstate-u.edu.ph>
2025-11-22 11:46:18 +0800

    Group4-MENDOZAKC: Add school_year column

--- schema.sql
+++ schema.sql
@@ -6,3 +6,4 @@
     FOREIGN KEY (class_id) REFERENCES classes(class_id)
 );
 INSERT INTO enrollments (enrollment_id, student_id, class_id), VALUES (01, 001, 2101), (02, 002, 2102), (03, 003, 2103); 
+ALTER TABLE enrollments ADD COLUMN school_year VARCHAR(100);
