ced0ee0f322646e35688340602448779bc683f93
KianTemplo <24-38254@g.batstate-u.edu.ph>
2025-11-22 14:36:50 +0800

    Fixed line 3 and 5

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE departments (dept_id INT PRIMARY KEY, dept_name VARCHAR (50));
-INSERT INTO departments (CICS VARCHAR(50), CABE VARCHAR(50));
+INSERT INTO departments (dept_id, dept_name) VALUES (1, 'CICS'), (2,'CABE');
 ALTER TABLE departments ADD COLUMN head_teacher VARCHAR(50);
-UPDATE departments SET head_teacher = 'Marielle Coronel' WHERE departments;
+UPDATE departments SET head_teacher = 'Marielle Coronel' WHERE dept_name = 'CICS';
