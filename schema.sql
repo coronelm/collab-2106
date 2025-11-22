1f99a57e4087a459f3543fbeea0050a0545406c3
Angelo-58 <24-39919@g.batstate-u.edu.ph>
2025-11-22 11:06:00 +0800

    Delete schema.sql

--- schema.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (01, 001, 2101), (02, 002, 2102), (03, 003, 2103); 
-ALTER TABLE enrollments ADD COLUMN school_year VARCHAR(100);
-ALTER TABLE enrollments ADD COLUMN school_year VARCHAR(100);
-ALTER TABLE enrollments ADD COLUMN school_year VARCHAR(100);
-ALTER TABLE users ADD COLUMN school_year VARCHAR(100);
-ALTER TABLE users ADD COLUMN school_year VARCHAR(100);
-ALTER TABLE users ADD COLUMN school_year VARCHAR(100);
-INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (01, 001, 2101), (02, 002, 2102), (03, 003, 2103); 
